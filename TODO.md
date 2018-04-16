a790995b24bf07f54dd6ab401008a365dac70720
Wong Jia Hau <hou32hou@gmail.com>
2018-04-16 23:42:49 +0800

    Make TestCalculateCharge to load data from text file

--- TODO.md
+++ TODO.md
@@ -5,12 +5,12 @@
 
 ## Part B
 - [ ] (a) Correctness of application code (2 classes).  
-- [ ] (b) Appropriate used of assertsXXX methods.  
-- [ ] (c) Using parameterised tests correctly  
-- [ ] (d) Invalid values are checked for in implemented code, and tests for invalid values are performed.  
+- [X] (b) Appropriate used of assertsXXX methods.  
+- [X] (c) Using parameterised tests correctly  
+- [X] (d) Invalid values are checked for in implemented code, and tests for invalid values are performed.  
 - [ ] (e) Use of test doubles, mocks, spies or stubs for testing.  
-- [ ] (f) Combining test cases into test suites  
-- [ ] (g) Setting up some tests so that test values are read from a text file instead of hardcoding into test code
+- [X] (f) Combining test cases into test suites  
+- [X] (g) Setting up some tests so that test values are read from a text file instead of hardcoding into test code
 - [ ] (h) Perform integration testing after unit tests have been completed
 
 
--- code/src/test/TestCalculateCharge.java
+++ code/src/test/TestCalculateCharge.java
@@ -2,14 +2,23 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.junit.Test;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import sun.misc.IOUtils;
+
 import org.junit.runner.RunWith;
 
 import code.CalculateCharge;
@@ -18,85 +27,58 @@ import code.HighQualityPaperOption;
 import code.PrintOption;
 import code.PrintRequest;
 
-
-@RunWith (JUnitParamsRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class TestCalculateCharge {
-	private Object[] getParams() {
-		HashSet<PrintOption> noOption         = 
-			new HashSet<PrintOption>();
-		HashSet<PrintOption> highQualityPaper = 
-			new HashSet<PrintOption>(Arrays.asList(new HighQualityPaperOption()));
-		HashSet<PrintOption> designEffect     = 
-			new HashSet<PrintOption>(Arrays.asList(new DesignEffectOption()));
-		HashSet<PrintOption> bothOptions      = 
-			new HashSet<PrintOption>(Arrays.asList(new HighQualityPaperOption(),new DesignEffectOption()));
+	Scanner inputDataScanner;
+	public TestCalculateCharge() throws Exception {
+		super();
+		File file = new File("TestCalculateCharge.data.txt");
+		this.inputDataScanner = new Scanner(file);
+	}
 
-		return new Object[] {
-			new Object[] {1  , noOption, 1.00},
-			new Object[] {4  , noOption, 1.00},
-			new Object[] {5  , noOption, 0.90},
-			new Object[] {10 , noOption, 0.90},
-			new Object[] {11 , noOption, 0.70},
-			new Object[] {20 , noOption, 0.70},
-			new Object[] {21 , noOption, 0.50},
-			new Object[] {50 , noOption, 0.50},
-			new Object[] {51 , noOption, 0.10},
-			new Object[] {100, noOption, 0.10},
-			new Object[] {1  , highQualityPaper, 1.10},
-			new Object[] {4  , highQualityPaper, 1.10},
-			new Object[] {5  , highQualityPaper, 1.00},
-			new Object[] {10 , highQualityPaper, 1.00},
-			new Object[] {11 , highQualityPaper, 0.80},
-			new Object[] {20 , highQualityPaper, 0.80},
-			new Object[] {21 , highQualityPaper, 0.60},
-			new Object[] {50 , highQualityPaper, 0.60},
-			new Object[] {51 , highQualityPaper, 0.20},
-			new Object[] {100, highQualityPaper, 0.20},
-			new Object[] {1  , designEffect, 1.10},
-			new Object[] {4  , designEffect, 1.10},
-			new Object[] {5  , designEffect, 1.00},
-			new Object[] {10 , designEffect, 1.00},
-			new Object[] {11 , designEffect, 0.80},
-			new Object[] {20 , designEffect, 0.80},
-			new Object[] {21 , designEffect, 0.60},
-			new Object[] {50 , designEffect, 0.60},
-			new Object[] {51 , designEffect, 0.20},
-			new Object[] {100, designEffect, 0.20},
-			new Object[] {1  , bothOptions, 1.20},
-			new Object[] {4  , bothOptions, 1.20},
-			new Object[] {5  , bothOptions, 1.10},
-			new Object[] {10 , bothOptions, 1.10},
-			new Object[] {11 , bothOptions, 0.90},
-			new Object[] {20 , bothOptions, 0.90},
-			new Object[] {21 , bothOptions, 0.70},
-			new Object[] {50 , bothOptions, 0.70},
-			new Object[] {51 , bothOptions, 0.30},
-			new Object[] {100, bothOptions, 0.30},
-		};
+	private Object[] getParams() {
+		HashSet<PrintOption> noOption = new HashSet<PrintOption>();
+		HashSet<PrintOption> highQualityPaper = new HashSet<PrintOption>(Arrays.asList(new HighQualityPaperOption()));
+		HashSet<PrintOption> designEffect = new HashSet<PrintOption>(Arrays.asList(new DesignEffectOption()));
+		HashSet<PrintOption> bothOptions = new HashSet<PrintOption>(
+				Arrays.asList(new HighQualityPaperOption(), new DesignEffectOption()));
+
+		ArrayList<Object> params = new ArrayList<Object>();
+		while(this.inputDataScanner.hasNextLine()) {
+			String[] data = this.inputDataScanner.nextLine().split(",");
+			ArrayList<Object> param =  new ArrayList<Object>();
+			param.add(Integer.parseInt(data[0].trim()));
+			param.add(
+				data[1].trim().equals("0") ? noOption : 
+				data[1].trim().equals("1") ? highQualityPaper :
+				data[1].trim().equals("2") ? designEffect :
+				data[1].trim().equals("3") ? bothOptions : null
+			);
+			param.add(Double.parseDouble(data[2].trim()));
+			params.add(param.toArray());
+		}
+		return params.toArray();
 	}
 
 	@Parameters(method = "getParams")
 	@Test
-	public void testBoundaries(int quantity, Set<PrintOption> options, double expectedChargePerPiece) 
-		throws Exception {
+	public void testBoundaries(int quantity, Set<PrintOption> options, double expectedChargePerPiece) throws Exception {
 		PrintRequest printRequest = new PrintRequest(quantity, options, null);
 		double result = new CalculateCharge().getChargePerPiece(printRequest);
 		assertEquals(expectedChargePerPiece, result, 0.0001);
 	}
 
-	@Test(expected=Exception.class)
+	@Test(expected = Exception.class)
 	public void testExceptions1_shouldThrowIfQuantityLessThanOne() throws Exception {
 		PrintRequest printRequest = new PrintRequest(0, null, null);
 		double result = new CalculateCharge().getChargePerPiece(printRequest);
-		
+
 	}
 
-	@Test(expected=Exception.class)
+	@Test(expected = Exception.class)
 	public void testExceptions2_shouldThrowIfQuantityMoreThanHundred() throws Exception {
 		PrintRequest printRequest = new PrintRequest(101, null, null);
 		double result = new CalculateCharge().getChargePerPiece(printRequest);
 	}
-
-
 
 }
